54e928711d316b086f1bbacfb87212198e4769f2
yhk8462 <45192660+yhk8462@users.noreply.github.com>
2023-09-14 16:27:58 +0700

    Main branch
    main branch

---
+++ RentalService.cpp
@@ -0,0 +1,25 @@
+/*
+  RMIT University Vietnam
+  Course: EEET2482/COSC2082
+  Semester: 2023-2
+  Group Assignment
+  Group: 25
+  Compiler used: Compiler version g++ 9.2.0
+  Created  date: 14/09/2023
+  Acknowledgement: 
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class RentalService
+{
+private:
+    /* data */
+public:
+    RentalService(/* args */);
+    ~RentalService();
+};
+
+
---
+++ User.cpp
@@ -0,0 +1,31 @@
+/*
+  RMIT University Vietnam
+  Course: EEET2482/COSC2082
+  Semester: 2023-2
+  Group Assignment
+  Group: 25
+  Compiler used: Compiler version g++ 9.2.0
+  Created  date: 14/09/2023
+  Acknowledgement: 
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class User
+{
+private:
+    /* data */
+public:
+    User(/* args */);
+    ~User();
+};
+
+class Member{};
+
+class NonMember{};
+
+class Admin{};
+
+
