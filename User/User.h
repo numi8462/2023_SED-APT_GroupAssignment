0cd14d1c759f55fb14396856b077002a1a7579ce
Xuan Thien <s3976442@rmit.edu.vn>
2023-09-19 14:23:07 +0700

    git commit -m "Add user class"

---
+++ User/User.h
@@ -0,0 +1,26 @@
+#ifndef DAPDIXAYLAI_USER_H
+#define DAPDIXAYLAI_USER_H
+
+
+
+//#include "../../System/System.h"
+
+
+#include <iostream>
+
+class System;
+
+class User {
+protected:
+    std::string username;
+    std::string password;
+
+public:
+    User(std::string username, std::string password);
+
+    friend class System;
+
+};
+
+
+#endif //DAPDIXAYLAI_USER_H
