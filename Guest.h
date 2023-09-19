69ffef7861ad14360fae2333879f61546a61b9d7
tringo135 <131856162+tringo135@users.noreply.github.com>
2023-09-19 16:12:22 +0700

    Guest.h

--- Guest.h
+++ Guest.h
@@ -8,3 +8,33 @@
   Created  date: 14/09/2023
   Acknowledgement: 
 */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Guest {
+      private:
+          string name;
+          int phoneNumber;
+      public:
+          // Getter method for name
+          string getName() {
+              return name;
+          }
+          
+          // Setter method for name
+          void setName(const string& newName) {
+              this->name = newName;
+          }
+          
+          // Getter method for phoneNumber
+          int getPhoneNumber() {
+              return phoneNumber;
+          }
+          
+          // Setter method for phoneNumber
+          void setPhoneNumber(int newPhoneNumber) {
+              this->phoneNumber = newPhoneNumber;
+          }
+};
