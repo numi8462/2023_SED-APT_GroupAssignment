a11f545d328ab6bf907e910636f53db05686de90
tringo135 <131856162+tringo135@users.noreply.github.com>
2023-09-19 11:45:13 +0700

    Request.h

--- Request.h
+++ Request.h
@@ -6,7 +6,7 @@
   Group: 25
   Compiler used: Compiler version g++ 9.2.0
   Created  date: 14/09/2023
-  Acknowledgement: 
+  Acknowledgement: https://codereview.stackexchange.com/questions/182480/date-class-in-c
 */
 
 #include <iostream>
@@ -17,25 +17,72 @@ using namespace std;
 
 class Member;
 class Motorbike;
+class Date {
+    private:
+        int year;
+        int month;
+        int day;
+    public:
+        // Default constructor
+        Date() {
+            time_t now = time(0);
+            tm* currentDate = localtime(&now);
+            year = currentDate->tm_year + 1950; // Adjust for year starting from 1950
+            month = currentDate->tm_mon + 1; // Adjust for month starting from 0
+            day = currentDate->tm_mday;
+        }
+        // Parameterized constructor
+        Date(int year, int month, int day) {
+            this->year = year;
+            this->month = month;
+            this->day = day;
+        }
+        
+        // Get the year value
+        int getYear() const {
+            return year;
+        }
+        //Get the month value
+        int getMonth() const {
+            return month;
+        }
+        //Get the day value
+        int getDay() const {
+            return day;
+        }
+};
 class Request {
     private:
         Member* renter;
         Motorbike* rentalBike;
-        time_t start;
-        time_t end;
+        Date start;
+        Date end;
         int credit;
         bool status;
     public:
-        Request(){};
-        Request(Member* renter,Motorbike* rentalBike,time_t start,time_t end,int credit,bool status) : renter(renter), rentalBike(rentalBike), start(start), end(end), credit(credit), status(status) {};
-
-        bool getStatus() { return status; };
+    //default constructor
+        Request(){
+          status = false;
+        };
+        
+    // Parameterized constructor
+        Request(Member renter, Motorbike rentalBike, Date startTime, Date endTime) {
+          this->renter = renter;
+          this->rentalBike = rentalBike;
+          this->startTime = startTime;
+          this->endTime = endTime;
+          status = false;
+        }
+    //Getter method to get the status
+        bool getStatus() { 
+            return status; 
+        };
         // sets the required amount of credit for rent
         void setCredit(int credit) {
           this->credit = credit;
         }
 
-        // sets reqeust status
+        // sets request status
         void setStatus(bool status) {
           this->status = status;
         }
